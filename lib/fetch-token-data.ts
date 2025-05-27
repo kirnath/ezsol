5d02330a74c3083a80af965ef8a662f26ff0b743
isaac <inr404@gmail.com>
2025-05-27 10:57:20 +0700

    fix tokens

--- lib/fetch-token-data.ts
+++ lib/fetch-token-data.ts
@@ -1,21 +1,15 @@
 import axios from "axios";
 
 async function fetchTokenOverview(mintAddress: string): Promise<any> {
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      "x-chain": "solana",
-      "X-API-KEY": "01665a81894141f49611cebffc1e5505",
-    },
-  };
+
 
   try {
     const response = await axios.get(
-      "https://pinata.ezsol.xyz/token-overview/" + mintAddress,
-      options
+      "https://pinata.ezsol.xyz/token-overview/" + mintAddress
     );
-    return response.data.data;
+    
+    return response.data;
+
   } catch (error) {
     console.error(error);
     throw error;
